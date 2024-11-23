9a243dac9c4403bae421e7b5ebd542160ea8c1f3
Flynn Bundy <flynn.sbundy@gmail.com>
2024-11-23 09:37:00 +0100

    fix exports

--- src/spec/fetchSpec.ts
+++ src/spec/fetchSpec.ts
@@ -2,7 +2,7 @@ import { readFile } from "fs/promises";
 
 export const fetchSpec = async (toolName: string) => {
     // Find the package in node_modules
-    const specPath = require.resolve(`industry-tools/tools/${toolName}/spec.json`, {
+    const specPath = require.resolve(`industry-tools/dist/tools/${toolName}/spec.json`, {
         paths: [process.cwd()]
     });
     const spec = await readFile(specPath, "utf8");
