ae8de8b3dee590bd811c936e3d33b5f3bb3a9525
Flynn Bundy <flynn.sbundy@gmail.com>
2024-11-23 15:57:40 +0100

    fix exports

--- src/spec/fetchSpec.ts
+++ src/spec/fetchSpec.ts
@@ -1,6 +1,16 @@
+import createEVMWallet from '../tools/Create_EVM_Wallet/spec.json';
+import deployEVMERC20Contract from '../tools/Deploy_EVM_ERC20_Contract/spec.json';
+import getEVMWallet from '../tools/Get_EVM_Wallet/spec.json';
+
+export const specs: Record<string, any> = {
+    "Create_EVM_Wallet": createEVMWallet,
+    "Get_EVM_Wallet": getEVMWallet,
+    "Deploy_EVM_ERC20_Contract": deployEVMERC20Contract,
+}
+
 export const fetchSpec = (toolName: string) => {
     try {
-        const spec = require(`industry-tools/dist/tools/${toolName}/spec.json`);
+        const spec = specs[toolName as keyof typeof specs];
         return spec;
     } catch (error) {
         throw new Error(`Failed to load spec for tool "${toolName}": ${error}`);
